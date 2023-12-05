b12e3b90292d8e693e47efba6ce6c25bc25f7021
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-12-04 21:12:07 -0600

    added userEventScript file

---
+++ netSuiteDev/src/FileCabinet/SuiteScripts/UserEventScript.js
@@ -0,0 +1,33 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType UserEventScript
+ */
+define(['N/record'],
+    /**
+ * @param{record} record
+ */
+    (record) => {
+        /**
+         * Defines the function definition that is executed before record is loaded.
+         */
+        const beforeLoad = (scriptContext) => {
+
+        }
+
+        /**
+         * Defines the function definition that is executed before record is submitted.
+         */
+        const beforeSubmit = (scriptContext) => {
+
+        }
+
+        /**
+         * Defines the function definition that is executed after record is submitted.
+         */
+        const afterSubmit = (scriptContext) => {
+
+        }
+
+        return {beforeLoad, beforeSubmit, afterSubmit}
+
+    });
