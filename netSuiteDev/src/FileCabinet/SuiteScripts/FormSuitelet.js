c75d9ad2d1a76fbc6f5a6794a3e26dff15192d75
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-12-18 23:49:40 -0600

    added form UI

--- netSuiteDev/src/FileCabinet/SuiteScripts/FormSuitelet.js
+++ netSuiteDev/src/FileCabinet/SuiteScripts/FormSuitelet.js
@@ -16,14 +16,81 @@ define(['N/ui/serverWidget'],
          */
         const onRequest = (context) => {
             const form = serverWidget.createForm({
-                title : 'First Suitelet Form'
+                title: 'First Suitelet Form'
             });
+
+            form.addResetButton({
+                label : 'Reset Button'
+            });
+
+            form.addSubmitButton({
+                label : 'Submit Button'
+            });
+
+            let fieldgroup = form.addFieldGroup({
+                id: 'custpage_usergroup',
+                label: 'User Information'
+            });
+
+            let fname = form.addField({
+                id : 'custpage_fname',
+                type : serverWidget.FieldType.TEXT,
+                label : 'First Name',
+                container: 'custpage_usergroup'
+            }).isMandatory= true;
+
+            let fLastName = form.addField({
+                id : 'custpage_flastname',
+                type : serverWidget.FieldType.TEXT,
+                label : 'Last Name',
+                container: 'custpage_usergroup'
+            }).isMandatory= true;
+
+            let fEmail = form.addField({
+                id : 'custpage_femail',
+                type : serverWidget.FieldType.EMAIL,
+                label : 'Email',
+                container: 'custpage_usergroup'
+            });
+
+            let sublist = form.addSublist({
+                id : 'custpage_sublistid',
+                type : serverWidget.SublistType.LIST,
+                label : 'Sublist'
+            });
+            sublist.addMarkAllButtons();
+
+            sublist.addField({
+                id : 'custpage_checkbox',
+                type : serverWidget.FieldType.CHECKBOX,
+                label : 'Select'
+            });
+
+            sublist.addField({
+                id : 'custpage_customer',
+                type : serverWidget.FieldType.SELECT,
+                label : 'Customer'
+            });
+
+            sublist.addField({
+                id : 'custpage_internalid',
+                type : serverWidget.FieldType.INTEGER,
+                label : 'Internal ID'
+            });
+
+            sublist.addField({
+                id : 'custpage_trannumber',
+                type : serverWidget.FieldType.INTEGER,
+                label : 'Transaction Number'
+            });
+
+
             context.response.writePage({
                 pageObject: form
             });
 
-        }
 
+        }
         return {onRequest}
 
     });
